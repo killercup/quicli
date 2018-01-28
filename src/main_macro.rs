8e4c4c4e5f04f7f6b82c15ffa9d73bd03dfd5780
Pascal Hertleif <killercup@gmail.com>
2018-01-28 20:53:29 +0100

    Make main! macro always set up logging
    I totally forgot about the last arm!

--- src/main_macro.rs
+++ src/main_macro.rs
@@ -1,7 +1,8 @@
 /// Quickly get a good `main` function
 ///
 /// Inside the block, you can write code using `?`. An `Ok(())` will
-/// automatically be appended.
+/// automatically be appended. The macro will also set up logging (using
+/// `env_logger`) automatically.
 ///
 /// # Parameters
 ///
@@ -11,14 +12,10 @@
 /// - `main!(|args: Cli| { ... })`: Automatically parses command line flags into
 ///   a struct `Cli` and binds it to `args`. (Basically, it prepends
 ///   `let args = Cli::from_args();`).
-/// - `main!(|args: Cli, log_level: verbosity| { ... })`: In addition to parsing
-///   the command line flags into `args` of type `Cli`, it will also initialize
-///   a logger.
-///
-///   The log level will depend on the integer value of the `verbosity` field of
-///   `args` (error = 0, warn = 1, info = 2, debug = 3, trace = ≥4). The field's
-///   type should be `u64`, so structopt will automatically give you its
-///   number of occurances.
+/// - `main!(|args: Cli, log_level: verbosity| { ... })`: The log level will
+///   depend on the integer value of the `verbosity` field of `args` (error = 0,
+///   warn = 1, info = 2, debug = 3, trace = ≥4). The field's type should be
+///   `u64`, so structopt will automatically give you its number of occurances.
 ///
 /// # Examples
 ///
@@ -94,6 +91,11 @@ macro_rules! main {
     ($body:expr) => {
         fn main() {
             fn run() -> $crate::prelude::Result<()> {
+                $crate::prelude::LoggerBuiler::new()
+                    .filter(Some(env!("CARGO_PKG_NAME")), $crate::prelude::LogLevel::Error.to_level_filter())
+                    .filter(None, $crate::prelude::LogLevel::Warn.to_level_filter())
+                    .try_init()?;
+
                 $body
                 Ok(())
             }
