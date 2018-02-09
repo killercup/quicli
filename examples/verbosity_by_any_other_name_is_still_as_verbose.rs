76f80d3060906cd739e9f7bc9ec705b28c30d863
Christopher Durham <cad97@cad97.com>
2018-02-09 18:30:47 -0500

    Make verbosity test a decent example

--- examples/verbosity_by_any_other_name_is_still_as_verbose.rs
+++ examples/verbosity_by_any_other_name_is_still_as_verbose.rs
@@ -10,9 +10,40 @@ struct Cli {
 }
 
 main!(|cli_args: Cli, log_level: team_rockets_blasting_off_again| {
+    println!("{} verbose flags passed", cli_args.team_rockets_blasting_off_again);
+
     error!("always shown");
+    if log_enabled!(Level::Error) {
+        println!("error logging enabled");
+    } else {
+        println!("error debugging disabled");
+    }
+
     warn!("shown at -v");
+    if log_enabled!(Level::Warn) {
+        println!("warn logging enabled");
+    } else {
+        println!("warn debugging disabled");
+    }
+
     info!("shown at -vv");
+    if log_enabled!(Level::Info) {
+        println!("info logging enabled");
+    } else {
+        println!("info debugging disabled");
+    }
+
     debug!("shown at -vvv");
+    if log_enabled!(Level::Debug) {
+        println!("debug logging enabled");
+    } else {
+        println!("debug debugging disabled");
+    }
+
     trace!("shown at -vvvv");
+    if log_enabled!(Level::Trace) {
+        println!("trace debugging enabled");
+    } else {
+        println!("trace debugging disabled");
+    }
 });
