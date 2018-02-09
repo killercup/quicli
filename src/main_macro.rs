26c13ca70497c935b2953a302bf5d85ae40b46c6
Pascal Hertleif <killercup@gmail.com>
2018-02-09 22:34:44 +0100

    Revert "feat: looser bounds on main! return type"
    This reverts commit 2ea8d3225c41111bd0e7fdad500c9e775dae66a1.

--- src/main_macro.rs
+++ src/main_macro.rs
@@ -32,7 +32,7 @@
 macro_rules! main {
     (|$args:ident: $cli:ty, log_level: $verbosity:ident| $body:expr) => {
         fn main() {
-            fn run() -> std::result::Result<(), Box<std::error::Error>> {
+            fn run() -> $crate::prelude::Result<()> {
                 let $args = <$cli>::from_args();
                 let log_level = match $args.verbosity {
                     0 => $crate::prelude::LogLevel::Error,
@@ -64,7 +64,7 @@ macro_rules! main {
 
     (|$args:ident: $cli:ty| $body:expr) => {
         fn main() {
-            fn run() -> std::result::Result<(), Box<std::error::Error>> {
+            fn run() -> $crate::prelude::Result<()> {
                 let $args = <$cli>::from_args();
                 $crate::prelude::LoggerBuiler::new()
                     .filter(Some(env!("CARGO_PKG_NAME")), $crate::prelude::LogLevel::Error.to_level_filter())
@@ -90,7 +90,7 @@ macro_rules! main {
 
     ($body:expr) => {
         fn main() {
-            fn run() -> std::result::Result<(), Box<std::error::Error>> {
+            fn run() -> $crate::prelude::Result<()> {
                 $crate::prelude::LoggerBuiler::new()
                     .filter(Some(env!("CARGO_PKG_NAME")), $crate::prelude::LogLevel::Error.to_level_filter())
                     .filter(None, $crate::prelude::LogLevel::Warn.to_level_filter())
