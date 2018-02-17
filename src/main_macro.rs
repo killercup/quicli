2364284b55d0f0c61d9fd493cddaa120254bc1e6
John Bledsoe <john.bledsoe@bolderhealthcare.com>
2018-02-17 08:40:54 -0600

    Fix macro names

--- src/main_macro.rs
+++ src/main_macro.rs
@@ -67,7 +67,7 @@ macro_rules! main {
         fn main() {
             fn run() -> $crate::prelude::Result<()> {
                 let $args = <$cli>::from_args();
-                $crate::prelude::LoggerBuiler::new()
+                $crate::prelude::LoggerBuilder::new()
                     .filter(Some(env!("CARGO_PKG_NAME")), $crate::prelude::LogLevel::Error.to_level_filter())
                     .filter(None, $crate::prelude::LogLevel::Warn.to_level_filter())
                     .try_init()?;
@@ -92,7 +92,7 @@ macro_rules! main {
     ($body:expr) => {
         fn main() {
             fn run() -> $crate::prelude::Result<()> {
-                $crate::prelude::LoggerBuiler::new()
+                $crate::prelude::LoggerBuilder::new()
                     .filter(Some(env!("CARGO_PKG_NAME")), $crate::prelude::LogLevel::Error.to_level_filter())
                     .filter(None, $crate::prelude::LogLevel::Warn.to_level_filter())
                     .try_init()?;
