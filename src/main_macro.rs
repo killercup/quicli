ce3bb58b47549cd415dd8f37bb89c0f8f91982ce
Markus Klein <markus.klein@blue-yonder.com>
2018-08-03 11:58:55 +0200

    update to failure 0.1.2

--- src/main_macro.rs
+++ src/main_macro.rs
@@ -44,7 +44,7 @@ macro_rules! main {
 
             if let Err(e) = run() {
                 eprintln!("error: {}", e);
-                for cause in e.causes().skip(1) {
+                for cause in e.iter_causes() {
                     eprintln!("caused by: {}", cause);
                 }
                 ::std::process::exit(1);
